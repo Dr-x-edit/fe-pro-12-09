6d577575fb3e0aaa4d8441082f9b7befefb03345
Volodymyr Shevchenko <shevchenko.v.vladimirovich@gmail.com>
2024-09-23 21:11:19 +0300

    HW-03-fe-pro-12-09: add other task

--- other_task/js/index.js
+++ other_task/js/index.js
@@ -3,44 +3,129 @@
 //Вираховуємо в якому підїзді та на якому поверсі знаходиться квартира
 // дім 9-ти поверховий, 5 підїздів на поверсі 4 квартири
 
-let floors = 9,
-  entrances = 5,
-  flats = 4;
+// let floors = 9,
+//   entrances = 5,
+//   flats = 4;
 
-let entranceNumberFlats = floors * flats;
-let totalNumberFlats = entranceNumberFlats * entrances;
+// let entranceNumberFlats = floors * flats;
+// let totalNumberFlats = entranceNumberFlats * entrances;
 
-// Перевірка розрахунків
+// // Перевірка розрахунків
 
-// console.log(totalNumberFlats);
-// console.log(entranceNumberFlats);
+// // console.log(totalNumberFlats);
+// // console.log(entranceNumberFlats);
 
-// Пропонуємо користувачу ввести номер квартири
+// // Пропонуємо користувачу ввести номер квартири
 
-let userFlat = +prompt("Please input number of flat:", 15);
+// let userFlat = +prompt("Please input number of flat:", 15);
 
-// Валідація введених даниї
+// // Валідація введених даниї
 
-while (userFlat > totalNumberFlats || userFlat < 1 || isNaN(userFlat)) {
-  userFlat = +prompt(`The flat is not define. Please try again`);
+// while (userFlat > totalNumberFlats || userFlat < 1 || isNaN(userFlat)) {
+//   userFlat = +prompt(`The flat is not define. Please try again`);
+// }
+
+// // Визначення підїзду в якому знаходиться квартира яку вказав користувач
+// let entrance = Math.ceil(userFlat / entranceNumberFlats);
+
+// console.log(`Your entrance is ${entrance <= 0 ? entrance + 1 : entrance}`);
+
+// // Визначаємо поверх на якому знаходиться квартира
+// console.log(
+//   `Your floor is ${Math.ceil(
+//     (userFlat - (entrance - 1) * entranceNumberFlats) / flats
+//   )}`
+// );
+
+// // Перебор всіх квартир із зазначенням підїзду
+
+// // for (let index = 1; index <= totalNumberFlats; index++) {
+// //   Math.ceil(index / entranceNumberFlats) <= 0
+// // ? console.log(`${index} - ${Math.ceil(index / entranceNumberFlats) + 1}`)
+// // : console.log(`${index} - ${Math.ceil(index / entranceNumberFlats)}`);
+// // }
+
+class highRiseBuilding {
+  constructor(floors, entrances, numberFlat) {
+    this.floors = floors;
+    this.entrances = entrances;
+    this.numberFlat = numberFlat;
+  }
+
+  _validInput(inputValue) {
+    while (
+      inputValue > this.totalNumberFlats ||
+      inputValue < 1 ||
+      isNaN(inputValue)
+    ) {
+      inputValue = +prompt(`Квартира не знайдена. Введіть іншу квартиру`);
+    }
+
+    return +inputValue;
+  }
+
+  _calcEntranceNumberFlats(consoleLog = true) {
+    this.entranceNumberFlats = this.floors * this.numberFlat;
+
+    if (consoleLog) {
+      console.log(`Всього квартир у під'їзді ${this.entranceNumberFlats}`);
+    }
+
+    return this;
+  }
+
+  _calcTotalNumberFlats(consoleLog = true) {
+    this.totalNumberFlats = this.entranceNumberFlats * this.entrances;
+    if (consoleLog) {
+      console.log(`Всього квартиру у будинку ${this.totalNumberFlats}`);
+    }
+    return this;
+  }
+
+  defineEntrance() {
+    this.userFlat = +prompt(`Введіть номер квартири:`);
+
+    this._calcEntranceNumberFlats(false);
+    this._calcTotalNumberFlats(false);
+    this.userFlat = this._validInput(this.userFlat);
+
+    this.entrance = Math.ceil(this.userFlat / this.entranceNumberFlats);
+
+    console.log(
+      `Ваша квартира знаходиться в ${
+        this.entrance < 1 ? this.entrance + 1 : this.entrance
+      } підїзді.`
+    );
+
+    return this;
+  }
+
+  defineFloor() {
+    this.userFlat = +prompt(`Введіть номер квартири:`);
+
+    this._calcEntranceNumberFlats(false);
+    this._calcTotalNumberFlats(false);
+    this.userFlat = this._validInput(this.userFlat);
+
+    this.floor = Math.ceil(
+      (this.userFlat - (this.entrance - 1) * this.entranceNumberFlats) /
+        this.numberFlat
+    );
+
+    console.log(`Ваша квартира знаходиться на ${this.floor} поверсі`);
+
+    return this;
+  }
 }
 
-// Визначення підїзду в якому знаходиться квартира яку вказав користувач
-let entrance = Math.ceil(userFlat / entranceNumberFlats);
+// let nineHighRiseBuilding = new highRiseBuilding(9, 5, 4);
 
-console.log(`Your entrance is ${entrance <= 0 ? entrance + 1 : entrance}`);
+// nineHighRiseBuilding
+//   ._calcEntranceNumberFlats()
+//   ._calcTotalNumberFlats()
+//   .defineEntrance(userFlat)
+//   .defineFloor(userFlat);
 
-// Визначаємо поверх на якому знаходиться квартира
-console.log(
-  `Your floor is ${Math.ceil(
-    (userFlat - (entrance - 1) * entranceNumberFlats) / flats
-  )}`
-);
+let fifeHighRiseBuilding = new highRiseBuilding(5, 4, 4);
 
-// Перебор всіх квартир із зазначенням підїзду
-
-// for (let index = 1; index <= totalNumberFlats; index++) {
-//   Math.ceil(index / entranceNumberFlats) <= 0
-// ? console.log(`${index} - ${Math.ceil(index / entranceNumberFlats) + 1}`)
-// : console.log(`${index} - ${Math.ceil(index / entranceNumberFlats)}`);
-// }
+fifeHighRiseBuilding.defineEntrance().defineFloor();
